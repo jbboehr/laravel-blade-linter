79dc4d4623b86c5202d595b07561524844822b4b
John Boehr <john@johnboehr.is>
2023-02-17 20:35:33 -0800

    Fix nix flake packages

--- flake.nix
+++ flake.nix
@@ -6,13 +6,16 @@
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     gitignore = {
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.gitignore.follows = "gitignore";
     };
   };
 
@@ -43,27 +46,83 @@
       }:
         enabled ++ [all.pcov]);
 
-      src = gitignore.lib.gitignoreSource ./.;
+      src = pkgs.lib.cleanSourceWith {
+        name = "laravel-blade-linter-source";
+        src = ./.;
+        filter = gitignore.lib.gitignoreFilterWith {
+          basePath = ./.;
+          extraRules = ''
+            .dockerignore
+            .codeclimate.json
+            .editorconfig
+            .envrc
+            .gitattributes
+            .github
+            .gitignore
+            *.nix
+            *.md
+          '';
+        };
+      };
+
+      makePackage = php': let
+        php = php'.withExtensions ({
+          enabled,
+          all,
+        }:
+          enabled ++ [all.ast]);
+      in
+        ((pkgs.callPackage ./composer-project.nix {
+            inherit php;
+            phpPackages = php.packages;
+          })
+          src)
+        .overrideAttrs (old: {
+          passthru.php = php;
+          preInstall = ''
+            export sourceRoot=laravel-blade-linter
+          '';
+          postInstall = ''
+            ln -s $out/libexec/laravel-blade-linter/bin/blade-linter $out/bin/blade-linter
+          '';
+          meta = {
+            mainProgram = "blade-linter";
+          };
+        });
+
+      makeCheck = package:
+        pkgs.runCommand "blah" {buildInputs = [package.php];} ''
+          cp -r --no-preserve=all ${package}/libexec/laravel-blade-linter/* .
+          ${pkgs.lib.getExe package.php} ./vendor/bin/phpunit | tee $out
+        '';
+
+      php81Package = makePackage pkgs.php81;
+      php82Package = makePackage pkgs.php82;
 
       pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        inherit src;
+        # we want to use the uncleaned src here...
+        src = ./.;
+        #inherit src;
         hooks = {
           actionlint.enable = true;
           alejandra.enable = true;
           alejandra.excludes = ["\/vendor\/"];
           markdownlint.enable = true;
-          #phpcs.enable = true;
+          phpcs.enable = true;
           shellcheck.enable = true;
         };
       };
     in rec {
-      packages = {
-        # something isn't working
-        # default = (pkgs.callPackage ./composer-project.nix { inherit php; }) src;
+      packages = rec {
+        php81 = php81Package;
+        php82 = php82Package;
+        default = php81;
       };
 
       checks = {
         inherit pre-commit-check;
+        php81 = makeCheck packages.php81;
+        php82 = makeCheck packages.php82;
       };
 
       devShells.default = pkgs.mkShell {
